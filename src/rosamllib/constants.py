75779d9f623231b2ae9ec62999ffeea4badf1dab
Yasin Abdulkadir <yasinaabdulkadir@gmail.com>
2025-06-16 18:10:42 -0700

    udpate non common tags list

--- src/rosamllib/constants.py
+++ src/rosamllib/constants.py
@@ -1,11 +1,19 @@
 # List of non-common tags
 non_common_tags = [
+    "0002|0003",  # MediaStorageSOPInstanceUID
+    "0008|0018",  # SOPInstanceUID
+    "0008|0032",  # Acquisition Time
+    "0008|0033",  # Content Time
+    "0008|1140",  # Referenced Image Sequence
+    "0008|2112",  # Source Image Sequence
+    "0020|0037",  # Image Orientation Patient
     "0020|0032",  # Image Position Patient
     "0020|1041",  # Slice Location
     "0020|0013",  # Instance Number
-    "0008|0032",  # Acquisition Time
-    "7FE0|0010",  # Pixel Data
     "0028|0030",  # Pixel Spacing
+    "0028|0106",  # Smallest Image Pixel Value
+    "0028|0107",  # Largest Image Pixel Value
+    "7FE0|0010",  # Pixel Data
 ]
 
 # Mapping of DICOM VR to Pandas data types
